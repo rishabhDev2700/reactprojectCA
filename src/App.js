2acf9bb9a268c7c5256c1f278ca91e6bfb2f08b9
rishabhDev2700 <rishavkumar2700@gmail.com>
2023-03-29 10:47:55 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import LoginScreen from './pages/LoginScreen'
+import ItemForm from './pages/ItemForm'
+import Orders from './pages/Orders'
+import CategoryForm from './pages/CategoryForm'
+import NotFound from './pages/NotFound'
+import OrderForm from './pages/OrderForm'
+import { createContext,useState } from 'react'
+import ClientForm from './pages/ClientForm'
+import Items from './pages/Items'
+export const AuthContext = createContext({})
+export default function App() {
+  const [user,setuser] = useState({});
+  console.log(user.email);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthContext.Provider value={{user,setuser}}>
+      <BrowserRouter>
+      
+      {user.loggedIn&&<Routes>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/add-item" element={<ItemForm/>}/>
+      <Route path="/items" element={<Items/>}/>
+      <Route path="/orders" element={<Orders/>}/>
+      <Route path="/add-category" element={<CategoryForm/>}/>
+      <Route path="/add-client" element={<ClientForm/>}/>
+      <Route path="/add-order" element={<OrderForm/>}/></Routes>}
+      <Routes>
+      <Route path="/" element={<LoginScreen/>}/>
+      {!user.loggedIn&&<Route path='*' element={<NotFound/>}/>}
+      </Routes>
+      </BrowserRouter>
+      </AuthContext.Provider>
     </div>
-  );
+  )
 }
-
-export default App;
